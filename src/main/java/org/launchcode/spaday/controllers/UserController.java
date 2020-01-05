1e88aa36c078ef3f4044414a1214dc0ed810012a
SarahF <sarahfrerichs@gmail.com>
2020-01-05 00:11:07 -0600

    finished 12.6.4 processing form submission completed

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +14,24 @@ public class UserController {
 
     @GetMapping("add")
     public String displayAddUserForm() {
-        return "add";
+        //TODO
+        System.out.println("add method reached--get");
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        System.out.println("add method reached--post " + user.getPassword() +" "+ verify);
+        model.addAttribute(user);
+        model.addAttribute(verify);
+        if (user.getPassword().equals(verify)) {
+            System.out.println("passwords match");
+        return "user/index";
+        } else {
+            System.out.println("passwords don't match");
+            return "user/add";
+        }
+
     }
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,17 @@
 package org.launchcode.spaday.models;
 
+import java.util.Objects;
+
 public class User {
     private String username;
     private String email;
     private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
@@ -27,5 +35,20 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username) &&
+                email.equals(user.email) &&
+                password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email, password);
     }
 }
